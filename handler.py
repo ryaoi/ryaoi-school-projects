ef31ce78845aa8cadabf724d5b0c5e53ba92524a
ryaoi42 <ryaoi@student.42.fr>
2019-04-11 21:55:48 +0200

    Add files via upload

--- handler.py
+++ handler.py
@@ -2,21 +2,10 @@ import json
 import requests
 import os
 
-def projects(event, context):
-
-    if 'path' not in event:
-        return {"statusCode": 400, "body": json.dumps("Missing id param!")}
-
-    params = event['path']
-
-    if 'id' not in params:
-        return {"statusCode": 400, "body": json.dumps("Missing id param!")}
-
-    if str(params['id']) != "20133":
-        return {"statusCode": 400, "body": json.dumps("Sorry but we only allow ryaoi's id :(")}
+def GetBearer():
 
     UID = os.environ['UID']
-    SECRET= os.environ['SECRET']
+    SECRET = os.environ['SECRET']
 
     payload = {'grant_type':'client_credentials', 'client_id':UID, 'client_secret': SECRET}
     try:
@@ -25,7 +14,21 @@ def projects(event, context):
         return {"statusCode": 500, "body": json.dumps("intra.42.fr endpoint error {}".format(str(e)))}
     tokenInfo = json.loads(body.content)
     bearer = tokenInfo['access_token']
+    return bearer
 
+def projects(event, context):
+
+
+    if 'path' not in event:
+        return {"statusCode": 400, "body": json.dumps("Missing id param!")}
+    params = event['path']
+
+    if 'id' not in params:
+        return {"statusCode": 400, "body": json.dumps("Missing id param!")}
+    if str(params['id']) not in ("20133", "40802"):
+        return {"statusCode": 400, "body": json.dumps("Sorry but we only allow ryaoi's id :(")}
+
+    bearer = GetBearer()
     session = requests.Session()
     session.headers.update({'Authorization': 'Bearer {}'.format(bearer)})
     try:
@@ -34,6 +37,7 @@ def projects(event, context):
         return {"statusCode": 500, "body": json.dumps("intra.42.fr/v2/user/{} endpoints error {}".format(params['id'], str(e)))}
 
     ryaoiInfo = json.loads(response.content)
+
     response = {
         "statusCode": 200,
         "Content-Type": "application/json",
